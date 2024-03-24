1e49609b710da82030813a9748ee70211a2a4c0d
Your Name <knnymrls@outlook.com>
2024-03-24 15:16:28 -0500

    Update transitions and easing in +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,8 @@
   import { TestStore } from "../stores";
   import TestCard from "../components/TestCard.svelte";
   import NavBar from "../components/NavBar.svelte";
-  import { fade, slide } from "svelte/transition";
+  import { slide, fade } from "svelte/transition";
+  import { cubicOut, cubicIn } from "svelte/easing";
 </script>
 
 <main class="px-24 py-8 h-svh bg-slate-100 flex flex-col gap-14">
@@ -10,7 +11,10 @@
   <h4 class="text-xl font-bold font-sans">Recent Tests</h4>
   <div class="flex flex-col gap-3">
     {#each $TestStore.slice(0, 4) as test (test.id)}
-      <div in:slide={{ duration: 200 }} out:slide={{ duration: 200 }}>
+      <div
+        in:slide={{ duration: 1000, delay: 1000, easing: cubicOut }}
+        out:slide={{ duration: 500, delay: 0, easing: cubicIn }}
+      >
         <TestCard {test} />
       </div>
     {/each}
