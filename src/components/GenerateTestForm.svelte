44e475acd7ae3aa2fbac4037a999bdaecf36f5b7
ZakRab <107758804+ZakRab@users.noreply.github.com>
2024-03-30 15:57:26 -0500

    Add test name input field and handle test data

--- src/components/GenerateTestForm.svelte
+++ src/components/GenerateTestForm.svelte
@@ -1,10 +1,15 @@
 <script>
-  import { createEventDispatcher } from "svelte";
-
+  import { Test } from "../model/Test"
+  import { TestStore } from "../stores";
+  import { MCQuestion } from "../model/mcQuestion";
+  import { LAQuestion } from "../model/laQuestion";
   let prompt = "";
-  const dispatch = createEventDispatcher();
-
+  let testName = "";
   async function handleSubmit() {
+    if ( !(prompt.length > 0 && testName.length > 0)){
+      alert("Please enter a prompt and a test name");
+      return;
+    }
     const response = await fetch("/generateTest", {
       method: "POST",
       body: JSON.stringify({ test: prompt }),
@@ -14,21 +19,59 @@
 
     let parsedData = parseData(data);
 
-    dispatch("submit", parsedData); // Emit an event with the data
-    console.log(parsedData);
     prompt = ""; // Reset prompt
+    testName = ""; // Reset prompt
+    handleData(parsedData);
   }
 
   function parseData(data) {
     let parsedData = data.chatCompletion.choices[0].message.content;
+    console.log(parsedData);
     return parsedData;
   }
+
+  function handleData(testData) {
+    // Parsing event data
+    if (typeof testData === "string") {
+      //FIXME: Make sure that it actually parses correctly because currently it is not.
+      testData = JSON.parse(testData);
+    }
+
+    // Assuming testData is structured correctly for a single MCQuestion for simplicity
+    // You might need to adjust this based on your actual data structure
+    const mcQuestion = new MCQuestion(
+      testData.question,
+      testData.options,
+      testData.answer
+    );
+
+    // Instantiate a Test object
+    //FIXME: Make sure that the Test object is instantiated correctly
+    const newTest = new Test(
+      testName,
+      Date.now(),
+      "Dynamic Tag",
+      [mcQuestion],
+      [new LAQuestion()]
+    );
+
+    // Updates the store with the new Test object
+    TestStore.update((currentTests) => [...currentTests, newTest]);
+  }
 </script>
+
+
 
 <form
   on:submit|preventDefault={handleSubmit}
   class="flex flex-col gap-2 items-center mt-10"
 >
+  <input
+    class="border-2 border-black rounded-md p-2 w-1/3"
+    type="text"
+    bind:value={testName}
+    placeholder="Enter a name for the test"
+  />
   <input
     class="border-2 border-black rounded-md p-2 w-1/3"
     type="text"
--- src/routes/generateTest/+page.svelte
+++ src/routes/generateTest/+page.svelte
@@ -1,44 +1,10 @@
 <script>
   import GenerateTestForm from "../../components/GenerateTestForm.svelte";
-  import { Test } from "../../model/Test";
-  import { TestStore } from "../../stores";
-  import { MCQuestion } from "../../model/mcQuestion";
-  import { LAQuestion } from "../../model/laQuestion";
   import NavBar from "../../components/NavBar.svelte";
 
-  function handleData(event) {
-    let testData = event.detail;
-
-    // Parsing event data
-    if (typeof testData === "string") {
-      //FIXME: Make sure that it actually parses correctly because currently it is not.
-      testData = JSON.parse(testData);
-    }
-
-    // Assuming testData is structured correctly for a single MCQuestion for simplicity
-    // You might need to adjust this based on your actual data structure
-    const mcQuestion = new MCQuestion(
-      testData.question,
-      testData.options,
-      testData.answer
-    );
-
-    // Instantiate a Test object
-    //FIXME: Make sure that the Test object is instantiated correctly
-    const newTest = new Test(
-      "Dynamic Test",
-      Date.now(),
-      "Dynamic Tag",
-      [mcQuestion],
-      [new LAQuestion()]
-    );
-
-    // Updates the store with the new Test object
-    TestStore.update((currentTests) => [...currentTests, newTest]);
-  }
 </script>
 
 <main class="px-24 py-8 h-svh bg-slate-100 flex flex-col gap-14">
   <NavBar></NavBar>
-  <GenerateTestForm on:submit={handleData}></GenerateTestForm>
+  <GenerateTestForm></GenerateTestForm>
 </main>
